2148a1f54627f2ad9840ba50a48dfa69dceb3ff9
Henry Phan <h.phan06@gmail.com>
2019-11-13 20:24:26 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,9 +1,10 @@
+require 'pry'
 # birthday_kids = {
 #   "Timmy" => 9, 
 #   "Sarah" => 6, 
 #   "Amanda" => 27
 # }
-require 'pry'
+
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
