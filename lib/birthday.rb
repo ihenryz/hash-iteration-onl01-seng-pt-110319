4e85a89b0ce8bb1db6932539a7e7b07410a24c83
Henry Phan <h.phan06@gmail.com>
2019-11-13 20:20:17 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -3,9 +3,15 @@
 #   "Sarah" => 6, 
 #   "Amanda" => 27
 # }
+require 'pry'
 
 def happy_birthday(birthday_kids)
-  # add your code snippet here!
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
 end
 
+def age_appropriate_birthday(birthday)
+  
+end
 
