df9e1c832c4b9ce5a5f02c7980e160155eb13cae
Henry Phan <h.phan06@gmail.com>
2019-11-13 20:33:36 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -15,6 +15,6 @@ end
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |names, age|
     if age < 12
-      puts 
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
 end
 
